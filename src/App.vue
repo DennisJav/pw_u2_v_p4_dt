1e24cae9f40cf594f16a9a6dd4e06d5c10968151
Dennisjav <denis.javier.tapia.ortiz@gmail.com>
2023-06-15 19:43:35 -0500

    taller17

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <HelloWorld msg="Copia de la parte de arriba"/>
 </template>
 
 <script>
@@ -10,6 +11,7 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+    
   }
 }
 </script>
